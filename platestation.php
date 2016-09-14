352aee43f135a87225634bf3e167a2c347f86adb
abcsharp <nullptr>
2016-09-14 10:13:29 +0900

    add platestation.php and hide admin bar

---
+++ platestation.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: PlateStation
+Plugin URI: 
+Description: 
+Version: 1.0.0
+Author:oxy
+Author URI: 
+License: GPL2
+*/
+
+add_filter('show_admin_bar', '__return_false');
+?>
