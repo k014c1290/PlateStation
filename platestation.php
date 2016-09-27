ba0e4635bac15c93ce0079729b650421b43c75d4
k014c1290 <k014c1290@it-neec.jp>
2016-09-27 14:29:29 +0900

    remove permission modification

--- platestation.php
+++ platestation.php
@@ -9,13 +9,8 @@ Author URI:
 License: GPL2
 */
 
-function add_permission(){
-	$role = get_role('author');
-	$role->add_cap('upload_files');
-}
-
 function add_feature_image($post){
-	if(get_post_type($post) === 'dish'){
+	if(get_post_type($post) == 'dish'){
 		$dish_image_id = get_field("dish_image", $post->ID);
 		set_post_thumbnail($post, $dish_image_id);
 	}
@@ -26,7 +21,5 @@ add_filter('new_to_publish', 'add_feature_image');
 add_filter('draft_to_publish', 'add_feature_image');
 add_filter('auto-draft_to_publish', 'add_feature_image');
 add_filter('pending_to_publish', 'add_feature_image');
-if (current_user_can('author') && !current_user_can('upload_files')){
-	add_action('admin_init', 'add_permission');
-}
+add_action('admin_init', 'add_permission');
 ?>
